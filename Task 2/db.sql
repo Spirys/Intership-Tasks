815581be46d75d99130e71510dc52568e55ef942
Spirys <spirys856@mail.ru>
2019-04-21 06:00:12 +0300

    db create script

---
+++ Task 2/db.sql
@@ -0,0 +1,8 @@
+﻿if not exists (select * from sysobjects where name='Files' and xtype='U')
+    create table Files (
+		Id int not null identity(1,1) primary key,
+		FileVersion varchar(32) not null,
+        Name varchar(100) not null,
+		DateTime DateTime not null
+    )
+go
